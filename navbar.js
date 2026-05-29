41666d2b9d8451ece1e5456c2eb38289a15c22b4
songwangxi <songwei315@126.com>
2026-05-29 19:56:34 +0800

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -5,7 +5,7 @@
         <div class="nav-links">
             <a href="/"><i class="fa fa-home"></i> 主页</a>
             <a href="/share/"><i class="fa fa-share-alt"></i> 分享留言</a>
-            <a href="https://用户名.freeflarum.com"><i class="fa fa-comments"></i> 论坛</a>
+            <a href="https://songwangxi.freeflarum.com"><i class="fa fa-comments"></i> 论坛</a>
         </div>
     </nav>`;
     document.body.insertAdjacentHTML('afterbegin', navHTML);
